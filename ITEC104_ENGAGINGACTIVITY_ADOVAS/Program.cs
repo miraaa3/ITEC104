3ae5d83c7439b0f81b681dbabe506c5c954c63a0
Miraaawr <112455464+miraaa3@users.noreply.github.com>
2022-09-15 16:57:26 +0800

    Add files via upload
    This our first engaging activity in ITEC104 under the supervision of Ms. Regina Rose Castillo.

---
+++ ITEC104_ENGAGINGACTIVITY_ADOVAS/Program.cs
@@ -0,0 +1,26 @@
+﻿public class ProgramStruct {
+    struct Employee {
+        public string firstName;
+        public string lastName;
+        public int Age;
+        public string CompletedTraining;
+    }
+    public static void Main(string[] args){
+
+        Employee employee1;
+
+        Console.WriteLine("What is your first name?");
+        employee1.firstName = Console.ReadLine( );
+        Console.WriteLine("What is your last name?");
+        employee1.lastName = Console.ReadLine( );
+        Console.WriteLine("How old are you?");
+        employee1.Age = int.Parse(Console.ReadLine( ));
+        Console.WriteLine("Do you complete your training?");
+        employee1.CompletedTraining = Console.ReadLine( );
+        Console.WriteLine("You entered");
+        Console.WriteLine("First Name: "+ employee1.firstName);
+        Console.WriteLine("Last Name: "+employee1.lastName);
+        Console.WriteLine("Age: "+employee1.Age);
+        Console.WriteLine("Completed the Training? "+employee1.CompletedTraining);
+        }
+}
