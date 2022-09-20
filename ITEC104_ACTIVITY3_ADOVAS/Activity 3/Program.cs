9d386748fec4c1a26a93471c65aed02e0775bb7a
Miraaawr <112455464+miraaa3@users.noreply.github.com>
2022-09-20 20:31:59 +0800

    Add files via upload

---
+++ ITEC104_ACTIVITY3_ADOVAS/Activity 3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace Activity_3
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			string[] emails = {"miracle@gmail.com","adovasm@gmail.com","miracleadovas@gmail.com"};
+			string[] usernames = {"Miracle A.","Adovas M.","Miracle Adovas"};
+			string[] passwords = {"miracle03","adovas23","miracleadovas23"};
+			
+			Console.Write("Index: ");
+			int index=int.Parse(Console.ReadLine());
+			
+			Console.WriteLine("\nEmail: {0}", emails[index]);
+			Console.WriteLine("Username: "+ usernames[index]);
+			Console.WriteLine("Password: "+ passwords[index]);
+			
+			Console.ReadKey();
+			Console.WriteLine("\nPress any key to Continue. . .");
+		}
+	}
+}
