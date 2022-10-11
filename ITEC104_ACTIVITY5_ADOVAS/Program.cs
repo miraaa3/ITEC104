f746661c03ebd65f4b59fd068ca0f7a3e2a6c0a9
Miraaawr <112455464+miraaa3@users.noreply.github.com>
2022-10-11 21:47:59 +0800

    Add files via upload

---
+++ ITEC104_ACTIVITY5_ADOVAS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Activity5
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            List<int> list = givenlist (new List<int> (new int[] {10, 20, 35, 65, 53, 48, 5, 1}));
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+        public static List<int> givenlist(List<int>nums)
+        {
+            return nums.Where(n => n % 10 < 5).ToList();
+        }
+    }
+}
