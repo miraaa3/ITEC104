7adee0cb28a404872ba23eb7c548570d8e130025
Miraaawr <112455464+miraaa3@users.noreply.github.com>
2022-10-21 14:41:00 +0800

    Add files via upload

---
+++ ITEC104_ACTIVITY6_ADOVAS/repos/Activity6/Activity6/Program.cs
@@ -0,0 +1,48 @@
+﻿HashSet <int> hash1 = new HashSet<int> ();
+
+for (int j = 0; j < 5; j++)
+{
+    Console.Write("Element: ");
+    hash1.Add(Convert.ToInt32(Console.ReadLine()));
+}
+
+Console.WriteLine();
+
+HashSet<int> hash2 = new HashSet<int>();
+
+for (int j = 0; j < 5; j++)
+{
+    Console.Write("Element: ");
+    hash2.Add(Convert.ToInt32(Console.ReadLine()));
+}
+
+HashSet<int> hash3 = new HashSet<int> (hash1);
+hash3.UnionWith(hash2);
+
+foreach (int j in hash3)
+{
+    Console.Write(j + " ");
+}
+
+Console.WriteLine ();
+
+HashSet<int> hash4 = new HashSet<int> (hash1);
+hash4.IntersectWith(hash2);
+
+foreach(int j in hash4)
+{
+    Console.Write(j + " ");
+}
+
+Console.WriteLine () ;  
+
+HashSet<int> hash5 = new HashSet<int> (hash1); 
+hash5.ExceptWith(hash2);
+
+
+foreach(int j in hash5)
+{
+    Console.Write(j + " ");
+}
+
+Console.ReadKey();
