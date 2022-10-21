eb8d1fa1427da698df609d05e2c6c399f734d6f0
Miraaawr <112455464+miraaa3@users.noreply.github.com>
2022-10-21 13:57:54 +0800

    Add files via upload

---
+++ ITEC104_ENGAGINGACTIVITY_ADOVAS/source/repos/EngagingActivity/EngagingActivity/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace CollectionsApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> st = new Stack<string>();
+            st.Push("A");
+            st.Push("R");
+            st.Push("I");
+            st.Push("M");
+            Console.WriteLine("Current stack: ");
+            foreach (string c in st)
+            {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine();
+            st.Push("T");
+            st.Push("Q");
+            Console.WriteLine("The next poppable value in stack: {0}", st.Peek());
+            Console.WriteLine("Current stack: ");
+            foreach (string c in st)
+            {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Removing values");
+            st.Pop();
+            st.Pop();
+            st.Pop();
+            Console.WriteLine("Current stack: ");
+            foreach (string c in st)
+            {
+                Console.Write(c + " ");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
